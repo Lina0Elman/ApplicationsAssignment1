65309596278f2cfdcf040fa5c7e5ac8bb6a124ab
lina elman <linael2002@gmail.com>
2024-12-25 22:19:45 +0200

    trying to add swagger & fixes of ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
-import express from "express";
-import commentsRoutes from "./routes/comments_routes";
-import postsRoutes from "./routes/posts_routes";
+import express from 'express';
+import commentsRoutes from './routes/comments_routes';
+import postsRoutes from './routes/posts_routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'API Documentation',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./routes/*.ts']
+};
+
+const specs = swaggerJsdoc(options);
 
 const app = express();
 
-const bodyParser = require("body-parser");
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use('/comments', commentsRoutes);
-app.use("/posts", postsRoutes);
+app.use('/posts', postsRoutes);
 
 export default app;
--- src/routes/comments_routes.ts
+++ src/routes/comments_routes.ts
@@ -7,6 +7,23 @@ const router: Router = express.Router();
 
 router.post('/', validateComment, (req: Request, res: Response) => commentsController.addComment(req, res));
 router.get('/', (req: Request, res: Response) => commentsController.getAllComments(req, res));
+
+/**
+ * @swagger
+ * /comments/{post_id}:
+ *   get:
+ *     summary: Get comments by post ID
+ *     parameters:
+ *       - in: path
+ *         name: post_id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the post
+ *     responses:
+ *       200:
+ *         description: List of comments
+ */
 router.get('/:post_id', (req: Request, res: Response) => commentsController.getCommentsByPostId(req, res));
 router.put('/:comment_id', validateCommentId, (req: Request, res: Response) => commentsController.updateComment(req, res));
 router.delete('/:comment_id', validateCommentId, (req: Request, res: Response) => commentsController.deleteComment(req, res));
